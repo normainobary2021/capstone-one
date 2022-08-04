df548a690572eed0d21bb96cd457364ff85ee2f7
normainobary2021 <norman.ainobushoborozi@gmail.com>
2022-08-04 20:24:00 +0300

    Modify main.js to remove linter errors

--- js/main.js
+++ js/main.js
@@ -1,105 +1,106 @@
-let stickyElement = document.querySelector('.desktop-nav');
+const stickyElement = document.querySelector('.desktop-nav');
 
-let speakerContainer = document.getElementById('speakerDiv');
+const speakerContainer = document.getElementById('speakerDiv');
 
-let seeMoreBtn = document.querySelector('#see-more');
-let expandButton = document.querySelector('.expand-button');
+const seeMoreBtn = document.querySelector('#see-more');
+const expandButton = document.querySelector('.expand-button');
 let currentItem = 2;
 
-let hamburgerIcon = document.getElementById('hamburger');
-let closeIcon = document.getElementById('closeX');
-let mobileMenu = document.getElementById('mobile-menu');
+const hamburgerIcon = document.getElementById('hamburger');
+const closeIcon = document.getElementById('closeX');
+const mobileMenu = document.getElementById('mobile-menu');
 
 hamburgerIcon.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
 });
 
-if(closeIcon) {
+if (closeIcon) {
   closeIcon.addEventListener('click', () => {
     mobileMenu.style.display = 'none';
   });
 }
 
 seeMoreBtn.addEventListener('click', () => {
-  let gridItems = [...document.querySelectorAll('.professional-grid .pro-grid-item')];
+  const gridItems = [...document.querySelectorAll('.professional-grid .pro-grid-item')];
   
-  for(let i = currentItem; i < currentItem + 2; i++) {
+  for (let i = currentItem; i < currentItem + 2; i=i+1) {
     gridItems[i].style.display = 'grid';
   }
 
   currentItem += 2;
 
-  if(currentItem >= gridItems.length) {
+  if (currentItem >= gridItems.length) {
     expandButton.style.display = 'none';
   }
 });
 
-let currStickyPos = stickyElement.getBoundingClientRect().top + window.pageYOffset;
+const currStickyPos = stickyElement.getBoundingClientRect().top + window.pageYOffset;
 
-window.onscroll = function() {
-    if(window.pageYOffset > currStickyPos) {
-        stickyElement.style.position = 'fixed';
-        stickyElement.style.top = '0px';
-    } else {
-        stickyElement.style.position = 'relative';
-        stickyElement.style.top = 'initial';
-    }
+window.onscroll = function () {
+  if (window.pageYOffset > currStickyPos) {
+    stickyElement.style.position = 'fixed';
+    stickyElement.style.top = '0px';
+  } 
+  else {
+    stickyElement.style.position = 'relative';
+    stickyElement.style.top = 'initial';
+  };
 }
 
 const speakers = [
-    {
-      name: 'Yochai Benkler',
-      photo: './images/p1.jpg',
-      role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
-    },
-    {
-      name: 'Kilnam Chon',
-      photo: './images/p2.jpg',
-      role: '',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
-    },
-    {
-      name: 'SohYeong Noh',
-      photo: './images/p3.jpg',
-      role: 'Director of Art Centre Nabi and Board Member Korea',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
-    },
-    {
-      name: 'Julia Leda',
-      photo: './images/p4.jpg',
-      role: 'System Trainer',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
-    },
-    {
-      name: 'Lila Tretikov',
-      photo: './images/p5.jpg',
-      role: 'Veterinary Doctor',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
-    },
-    {
-      name: 'Ryan Merkley',
-      photo: './images/p1.jpg',
-      role: 'Software Engineer, Google Inc.',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
-    },
-  ];
+  {
+    name: 'Yochai Benkler',
+    photo: './images/p1.jpg',
+    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
+  },
+  {
+    name: 'Kilnam Chon',
+    photo: './images/p2.jpg',
+    role: '',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
+  },
+  {
+    name: 'SohYeong Noh',
+    photo: './images/p3.jpg',
+    role: 'Director of Art Centre Nabi and Board Member Korea',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
+  },
+  {
+    name: 'Julia Leda',
+    photo: './images/p4.jpg',
+    role: 'System Trainer',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
+  },
+  {
+    name: 'Lila Tretikov',
+    photo: './images/p5.jpg',
+    role: 'Veterinary Doctor',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
+  },
+  {
+    name: 'Ryan Merkley',
+    photo: './images/p1.jpg',
+    role: 'Software Engineer, Google Inc.',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates ipsum corporis nam magnam, quia nisi molestiae temporibus reiciendis officiis?',
+  },
+];
   
-  const createSpeakers = () => {
-    let eventSpeakers = '';
-    speakers.forEach((eventSpeaker) => {
-    eventSpeakers += `<div class="pro-grid-item"><div class="pro-grid-images">
-      <img class="trans-grid" src="./images/bg-transparent.jpeg" alt="Transparent">
-      <img class="pro-image" src="${eventSpeaker.photo}" alt="${eventSpeaker.name} profile">
-        </div>
-        <div class="pro-text">
-            <h3>${eventSpeaker.name}</h3>
-            <h4>${eventSpeaker.role}</h4>
-            <div class="gray-line"></div>
-            <p>${eventSpeaker.text}</p>
-        </div></div>`;
-    });
-    speakerContainer.innerHTML = eventSpeakers;
-  };
+const createSpeakers = () => {
+  let eventSpeakers = '';
+  speakers.forEach((eventSpeaker) => {
+  eventSpeakers += `<div class="pro-grid-item"><div class="pro-grid-images">
+    <img class="trans-grid" src="./images/bg-transparent.jpeg" alt="Transparent">
+    <img class="pro-image" src="${eventSpeaker.photo}" alt="${eventSpeaker.name} profile">
+      </div>
+      <div class="pro-text">
+          <h3>${eventSpeaker.name}</h3>
+          <h4>${eventSpeaker.role}</h4>
+          <div class="gray-line"></div>
+          <p>${eventSpeaker.text}</p>
+      </div></div>`;
+  });
+  speakerContainer.innerHTML = eventSpeakers;
+};
 
 createSpeakers();
